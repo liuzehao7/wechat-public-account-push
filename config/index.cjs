38c9d883e5ab583d5a0e6de158f01fb410482977
liuzehao7 <134271875+liuzehao7@users.noreply.github.com>
2023-05-23 15:39:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -14,8 +14,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '山西',
+  CITY: '太原',
 
   USERS: [
     {
@@ -30,14 +30,14 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '宝贝', year: '1998', date: '05-28',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '06-22',
+          type: '生日', name: '小鱼', year: '1998', date: '06-22',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2023', date: '03-06',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
