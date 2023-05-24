623ef677f000295d9f8a07bfde0f3382c908f1c0
liuzehao7 <134271875+liuzehao7@users.noreply.github.com>
2023-05-24 15:58:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -30,7 +30,7 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1998', date: '05-28',
+          type: '*生日', name: '玉杰', year: '1998', date: '05-28',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
@@ -43,7 +43,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2023-04-17' },
+        { keyword: 'love_day', date: '2023-04-16' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
